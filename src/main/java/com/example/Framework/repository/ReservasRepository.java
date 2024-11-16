efb3140bbbd24b615e55b17e0d2cbfab88543fb4
Gerson Vásquez <gerson.vasquez23@itca.edu.sv>
2024-11-16 10:28:50 +0530

    Repositorio Reservas

---
+++ src/main/java/com/example/Framework/repository/ReservasRepository.java
@@ -0,0 +1,7 @@
+package com.example.Framework.repository;
+
+import com.example.Framework.entity.Reservas;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReservasRepository extends JpaRepository<Reservas, Long> {
+}
