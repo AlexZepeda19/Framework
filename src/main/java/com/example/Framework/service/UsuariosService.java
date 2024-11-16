4020cb6f2a90be5a4d7be9c8f6fb278f55e96118
Gerson Vásquez <gerson.vasquez23@itca.edu.sv>
2024-11-16 11:15:49 +0530

    Servicio Usuarios

---
+++ src/main/java/com/example/Framework/service/UsuariosService.java
@@ -0,0 +1,33 @@
+package com.example.Framework.service;
+
+import com.example.Framework.entity.Usuarios;
+import com.example.Framework.repository.UsuariosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuariosService {
+
+    @Autowired
+    private UsuariosRepository usuariosRepository ;
+
+    public List<Usuarios> getUsuarios() {
+        return usuariosRepository.findAll();
+    }
+
+    public Optional<Usuarios> getUsuarios(Long id) {
+        return usuariosRepository.findById(id);
+    }
+
+    public void saveOrUpdate(Usuarios usuarios) {
+        usuariosRepository.save(usuarios);
+    }
+
+    public void deleteById(Long id) {
+        usuariosRepository.deleteById(id);
+    }
+
+}
