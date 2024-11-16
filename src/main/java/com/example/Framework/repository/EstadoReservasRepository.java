df401012d137a92becebb2cf09b6cde6179c69b4
Gerson Vásquez <gerson.vasquez23@itca.edu.sv>
2024-11-16 10:31:12 +0530

    Repositorio de EstadoReservas y HistorialPedidos

---
+++ src/main/java/com/example/Framework/repository/EstadoReservasRepository.java
@@ -0,0 +1,7 @@
+package com.example.Framework.repository;
+
+import com.example.Framework.entity.EstadoReservas;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstadoReservasRepository extends JpaRepository<EstadoReservas, Long> {
+}
---
+++ src/main/java/com/example/Framework/repository/HistorialPedidosRepository.java
@@ -0,0 +1,7 @@
+package com.example.Framework.repository;
+
+import com.example.Framework.entity.HistorialPedidos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HistorialPedidosRepository extends JpaRepository <HistorialPedidos, Long> {
+}
