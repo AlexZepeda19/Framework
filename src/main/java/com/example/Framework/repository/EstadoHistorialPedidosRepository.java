e0929c1cf130ade83779a160583891df5f09e94e
Manuel Zepeda <168386278+AlexZepeda19@users.noreply.github.com>
2024-11-16 10:15:51 -0600

    Repositorio para tabla Estado historial pedido y libros

---
+++ src/main/java/com/example/Framework/repository/EstadoHistorialPedidosRepository.java
@@ -0,0 +1,7 @@
+package com.example.Framework.repository;
+
+import com.example.Framework.entity.EstadoHistorialPedidos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstadoHistorialPedidosRepository extends JpaRepository<EstadoHistorialPedidos, Long> {
+}
---
+++ src/main/java/com/example/Framework/repository/LibrosRepository.java
@@ -0,0 +1,7 @@
+package com.example.Framework.repository;
+
+import com.example.Framework.entity.Libros;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LibrosRepository extends JpaRepository<Libros, Long>{
+}
