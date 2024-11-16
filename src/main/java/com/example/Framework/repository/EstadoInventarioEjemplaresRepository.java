fab516efa509e83bbb29ee2e75cf9537279aa7aa
Gerson Vásquez <gerson.vasquez23@itca.edu.sv>
2024-11-16 10:27:15 +0530

    Repositorio de EstadoInventarioEjemplares

---
+++ src/main/java/com/example/Framework/repository/EstadoInventarioEjemplaresRepository.java
@@ -0,0 +1,7 @@
+package com.example.Framework.repository;
+
+import com.example.Framework.entity.EstadoInventarioEjemplares;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstadoInventarioEjemplaresRepository extends JpaRepository<EstadoInventarioEjemplares, Long> {
+}
