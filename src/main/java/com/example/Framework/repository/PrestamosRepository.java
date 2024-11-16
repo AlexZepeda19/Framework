58e65c8d37d0ed1ad7870822103b4f318a54d2fb
Manuel Zepeda <168386278+AlexZepeda19@users.noreply.github.com>
2024-11-16 10:23:39 -0600

    Repositorios Prestamo y Estado prestamo

---
+++ src/main/java/com/example/Framework/repository/PrestamosRepository.java
@@ -0,0 +1,7 @@
+package com.example.Framework.repository;
+
+import com.example.Framework.entity.Prestamos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PrestamosRepository extends JpaRepository<Prestamos, Long> {
+}
